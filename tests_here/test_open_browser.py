42f3aa0e353f087537faaafdf88e1ba7dcf82abc
okysil <oleksandr.kysil@okta.com>
2020-11-22 16:30:04 +0200

    Second Commit

--- tests_here/test_open_browser.py
+++ tests_here/test_open_browser.py
@@ -10,3 +10,13 @@ def test_open_browser(driver_session):
     :return:
     """
     pass
+
+
+@pytest.mark.test
+def test_open_apple(driver_session):
+    """
+    TEST
+    :param driver_session:
+    :return:
+    """
+    driver_session.get("https://www.apple.com")
